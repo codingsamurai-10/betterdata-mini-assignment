a87662cbfd861ac3eb99e3eef345723a00e76dda
Aditya Rana <official.adityarana01@gmail.com>
2021-10-11 16:50:50 +0530

    Connecting to database

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const logger = require("morgan");
 
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -18,5 +20,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to db"));
 
 module.exports = app;
