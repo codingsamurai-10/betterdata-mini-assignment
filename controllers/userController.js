300e367d9f4b144a95f4a4b9abcf39ab89250e3f
Aditya Rana <official.adityarana01@gmail.com>
2021-10-11 17:09:06 +0530

    Added creating new user functionality

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+const userModel = require("../models/userModel");
+
+const addNewUser = (req, res) => {
+  const user = new userModel({ ...req.body });
+  user.save();
+  res.send("ok");
+};
+module.exports = { addNewUser };
