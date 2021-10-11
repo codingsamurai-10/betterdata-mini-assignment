9309f6e67c1a784fc210b8e51eac970e17f9e915
Aditya Rana <official.adityarana01@gmail.com>
2021-10-12 00:43:00 +0530

    Removed unused package

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 require("dotenv").config();
@@ -17,7 +16,6 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 const db = mongoose
