712021c7f248bdf1b3377eae17e10a6cd5f2dcd4
Aditya Rana <official.adityarana01@gmail.com>
2021-10-11 20:22:06 +0530

    Implemented route for adding model to a given project of a given user

--- controllers/model.js
+++ controllers/model.js
@@ -1,3 +1,13 @@
-const addNewModel = async (req, res) => {};
+const userModel = require("../models/userModel");
+
+const addNewModel = async (req, res) => {
+  const userId = req.body.userId;
+  const projectId = req.body.projectId;
+  const user = await userModel.findById(userId);
+  const project = user.projects.id(projectId);
+  project.models.push(req.body.model);
+  user.save();
+  res.send("ok");
+};
 
 module.exports = { addNewModel };
