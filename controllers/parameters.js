daa0530890d7b0751d8f72da779989fa71eeb434
Aditya Rana <official.adityarana01@gmail.com>
2021-10-11 22:33:18 +0530

    Added function to fetch parameters of a given model

---
+++ controllers/parameters.js
@@ -0,0 +1,10 @@
+const userModel = require("../models/userModel");
+
+const getParametersOfModel = async (req, res) => {
+  const user = await userModel.findById(req.body.userId);
+  const project = user.projects.id(req.body.projectId);
+  const model = project.models.id(req.body.modelId);
+  res.send(model.parameters);
+};
+
+module.exports = { getParametersOfModel };
