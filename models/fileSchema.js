c917e17383d24497e8bcfbc48efec03fa013d595
Aditya Rana <official.adityarana01@gmail.com>
2021-10-12 20:43:08 +0530

    Renamed imageSchema to fileSchema. Added schema for holding file metadata.

---
+++ models/fileSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  fileName: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+  mimetype: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = schema;
