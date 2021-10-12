803841ed3aa576a8967b4b419a19ed6a3c36b02a
Aditya Rana <official.adityarana01@gmail.com>
2021-10-12 20:39:53 +0530

    Added multer config to append filename

--- db.js
+++ db.js
@@ -1,6 +1,20 @@
 const mongoose = require("mongoose");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const path = require("path");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    crypto.randomBytes(16, (err, buf) => {
+      const filename = buf.toString("hex") + path.extname(file.originalname);
+      cb(null, filename);
+    });
+  },
+});
+const upload = multer({ storage: storage });
 
 mongoose
   .connect(process.env.MONGODB_URI)
