6cc4be92cc17d4224b58df3aa77e443ff878b534
Aditya Rana <official.adityarana01@gmail.com>
2021-10-12 19:51:44 +0530

    Moving from gridfs to local storage

--- db.js
+++ db.js
@@ -1,38 +1,8 @@
 const mongoose = require("mongoose");
 const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const crypto = require("crypto");
-const path = require("path");
+const upload = multer({ dest: "uploads/" });
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to db"));
-const db = mongoose.connection;
-let gfs;
-db.once("open", () => {
-  gfs = Grid(db, mongoose.mongo);
-  gfs.collection("uploads");
-});
-
-const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString("hex") + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads",
-        };
-        resolve(fileInfo);
-      });
-    });
-  },
-});
-const upload = multer({ storage });
-
-module.exports = { gfs, upload };
+module.exports = { upload };
