de3b0f9bf714f977a3f2cec761144e8044882a02
Aditya Rana <official.adityarana01@gmail.com>
2021-10-12 22:01:06 +0530

    Added synthetic-data to schema

--- models/modelSchema.js
+++ models/modelSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const parameterSchema = require("./parameterSchema");
+const fileSchema = require("./fileSchema");
 
 const schema = new mongoose.Schema({
   name: {
@@ -8,5 +9,6 @@ const schema = new mongoose.Schema({
     // TODO: validator to keep model names unique
   },
   parameters: parameterSchema,
+  syntheticData: [fileSchema],
 });
 module.exports = schema;
